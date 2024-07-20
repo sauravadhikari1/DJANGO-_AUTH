ce2b5036667ca5f08a49f83170551fc025a99967
sauravadhikari1 <sauravadhikari456@gmail.com>
2024-07-20 19:47:16 +1000

    django project

---
+++ website/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from.import views
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home',views.home, name='home'),
+    path('sign-up',views.signup, name='sign-up'),
+    path('create-post',views.create_post, name='create_post'),
+    
+]
